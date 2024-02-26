19f8de03b2775f562e458ae94de5164c36e2015d
Lia Cardoso <lia.cardoso@ua.pt>
2024-02-26 22:10:03 +0000

    ex1 done

--- lab2/lab2_1/src/main/java/ua/deti/tqs/StockPortfolio.java
+++ lab2/lab2_1/src/main/java/ua/deti/tqs/StockPortfolio.java
@@ -1,27 +1,27 @@
 package ua.deti.tqs;
 
+import java.util.ArrayList;
 import java.util.List;
 
-public class StockPortfolio implements IStockmarketService{
+public class StockPortfolio{
     private IStockmarketService stockmarket;
     private List<Stock> stocks;
 
     public StockPortfolio(IStockmarketService stockmarket, List<Stock> stocks){
         this.stockmarket = stockmarket;
-        this.stocks = stocks;
+        this.stocks = new ArrayList<>();
     }
 
     public void addStock(Stock stock){
-
+        this.stocks.add(stock);
     }
 
     public double totalValue(){
-        double total_value = 0;
+        double total_value = 0.0;
+        for(Stock stock: stocks){
+            total_value += stockmarket.lookUpPrice(stock.getLabel())*stock.getQuantity();
+        }
         return total_value;
     }
 
-    public double lookUpPrice(String label){
-        double price = 0;
-        return price;
-    }
 }
