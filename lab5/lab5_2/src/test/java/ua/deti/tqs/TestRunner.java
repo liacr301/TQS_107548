b1fefad56603efe5a59d325e3b14b8119a7d08f1
Lia Cardoso <lia.cardoso@ua.pt>
2024-03-17 17:31:22 +0000

    changes in 5_2

--- lab5/lab5_2/src/test/java/ua/deti/tqs/TestRunner.java
+++ lab5/lab5_2/src/test/java/ua/deti/tqs/TestRunner.java
@@ -1,16 +1,16 @@
 package ua.deti.tqs;
+import static io.cucumber.junit.platform.engine.Constants.GLUE_PROPERTY_NAME;
+import static io.cucumber.junit.platform.engine.Constants.PLUGIN_PROPERTY_NAME;
+
 import org.junit.platform.suite.api.ConfigurationParameter;
 import org.junit.platform.suite.api.IncludeEngines;
 import org.junit.platform.suite.api.SelectClasspathResource;
 import org.junit.platform.suite.api.Suite;
 
-import static io.cucumber.junit.platform.engine.Constants.PLUGIN_PROPERTY_NAME;
-import static io.cucumber.junit.platform.engine.Constants.GLUE_PROPERTY_NAME;
-
 @Suite
 @IncludeEngines("cucumber")
-@SelectClasspathResource("ua/deti/tqs/lab5")
+@SelectClasspathResource("io/cucumber")
 @ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "pretty")
-@ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "ua.deti.tqs.lab5")
-public class BookSearchTest {
+@ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "io.cucumber")
+public class TestRunner {
 }
