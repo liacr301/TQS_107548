4a555df11234a8d5052ae8e8cca77fc183501aa9
Lia Cardoso <lia.cardoso@ua.pt>
2024-03-17 17:32:46 +0000

    setup lab5_3

---
+++ lab5/lab5_3/src/test/java/ua/deti/tqs/BlazeDemoTest.java
@@ -0,0 +1,15 @@
+package ua.deti.tqs;
+
+import static io.cucumber.junit.platform.engine.Constants.GLUE_PROPERTY_NAME;
+
+import org.junit.platform.suite.api.ConfigurationParameter;
+import org.junit.platform.suite.api.IncludeEngines;
+import org.junit.platform.suite.api.SelectClasspathResource;
+import org.junit.platform.suite.api.Suite;
+
+@Suite
+@IncludeEngines("cucumber")
+@SelectClasspathResource("io/github/bonigarcia")
+@ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "io.github.bonigarcia")
+public class BlazeDemoTest{
+}
