e9889c992cd698adc48e4289a1ab0dda50b81433
Lia Cardoso <lia.cardoso@ua.pt>
2024-04-02 16:41:33 +0100

    added num seats

--- hw1/infos_bus/generate_info.py
+++ hw1/infos_bus/generate_info.py
@@ -9,23 +9,23 @@ def gerar_datas_aleatorias(n):
     base = datetime.now()
     return [(base + timedelta(days=random.randint(0, 10))).strftime('%Y-%m-%d') for _ in range(n)]
 
-# Informações iniciais para gerar as viagens
 viagens = []
 viagem_id = 1
 
 for origem in cidades:
     for destino in cidades:
-        if origem != destino:  # Garantir que a origem e o destino sejam diferentes
-            # Criar 5 viagens para cada par de origem/destino
+        if origem != destino:  
             datas_aleatorias = gerar_datas_aleatorias(5)
             for i in range(1, 6):
                 bus_number = random.randint(100, 600)
+                num_seats = random.randint(20, 40)
                 horarios = [{
                     "ID": f"{viagem_id:03}",
                     "Bus": f"Bus {bus_number}",
                     "Date": f"{datas_aleatorias[i-1]}",  
-                    "Dia de Hour": f"{8 + i}:00",  # Horários diferentes para cada viagem                    
-                    "Preço": f"{20 + i * 5}€"  # Preço diferente para cada viagem
+                    "Dia de Hour": f"{8 + i}:00",                  
+                    "Preço": f"{20 + i * 5}€",
+                    "Seats": f"{num_seats}"
                 }]
                 viagem = {
                     "from": origem,
@@ -38,9 +38,9 @@ for origem in cidades:
 jsonbus = {
     "busdemo": viagens
 }
-# Converter a lista de viagens para JSON
+
 viagens_json = json.dumps(jsonbus, indent=4)
 with open("bus.json", "w") as arquivo:
     arquivo.write(viagens_json)
-# Mostrar o JSON criado
+
 print(viagens_json)
