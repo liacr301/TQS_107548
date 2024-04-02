34ee416829be85e056ba52d83b98a6a8b3f6ba1b
Lia Cardoso <lia.cardoso@ua.pt>
2024-04-02 22:27:21 +0100

    reservation repository

--- hw1/backend/src/main/java/ua/deti/tqs/backend/dao/ReservationRepository.java
+++ hw1/backend/src/main/java/ua/deti/tqs/backend/dao/ReservationRepository.java
@@ -1,5 +1,10 @@
 package ua.deti.tqs.backend.dao;
 
-public class ReservationRepository {
-    
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import ua.deti.tqs.backend.models.Reservation;
+
+@Repository
+public interface ReservationRepository extends JpaRepository<Reservation, String> {
 }
